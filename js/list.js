496453402726b0d31b41445caae9a6fb6b571510
tarkomate99 <tarkomate99@gmail.com>
2021-10-11 13:21:52 +0200

    scss

--- js/list.js
+++ js/list.js
@@ -6,6 +6,19 @@ $.getJSON("https://driveral.com/hu/api/jobs?uid=3e7ccfff-eab8-40b3-b055-98fbd40c
 function appendData(data) {
     var main = document.getElementById('jobs-list');
 
+    const appendCol = ( colName, data, icon, text = "" ) => {
+        console.log(colName, data);
+
+        const _icon = ( icon ? icon : "" );
+        const _text = ( text ? text : "" );
+
+        if( data && typeof(data) !== "undefined" ) {
+            return `<div class="col-md-4 col-lg-3 job-${colName}">${_icon}${data} ${text}</div>`
+        }
+
+        return "";
+    }
+
     let jobsHTML = "";
     for (var job of data) {
         jobsHTML += `
@@ -15,15 +28,19 @@ function appendData(data) {
                         <div class="col-lg-2 imagebox"><img src="${job.company_image}" alt="job-img" class="image"/></div>
                             <div class="col-lg-10 d-flex align-items-center">
                                 <div class="row ml-l">
-                                <div class="col-12 job-title">${job.title}
-                                    <div class="col-2 job-button"><button class="btn btn btn-outline-primary float-right job-time-button">Full Time</button></div>
+                                <div class="col-12 job-title"> 
+                                    <div class="row">
+                                        <div class="col-lg-9 col-12"><a href="${job.slug}">${job.title}</a></div>
+                                        <div class="col-lg-3 col-12 job-button"><button class="btn btn btn-outline-primary float-right job-time-button">Full Time</button></div>
+                                    </div>
+                                    
                                 </div>
                                     <div class="col">
                                     <div class="row">
-                                        <div class="col-md-4 col-lg-3 job-company_name">${job.company_name}</div>
-                                        <div class="col-md-4 col-lg-3 job-license">${job.license} permis de conducere</div>
-                                        <div class="col-md-4 col-lg-3 job-country"><i class="fas fa-map-marker-alt"></i>${job.country}</div>
-                                        <div class="col-md-4 col-lg-3 job-payment"><i class="fas fa-dollar-sign"></i>${job.payment}</div>
+                                        ${appendCol( "company_name", job.company_name )}
+                                        ${appendCol( "license", job.license, "", "licență" )}
+                                        ${appendCol( "country", job.country, '<i class="fas fa-map-marker-alt"></i>' )}
+                                        ${appendCol( "payment", job.payment, '<i class="fas fa-dollar-sign"></i>' )}
                                     </div>
                                 </div>
                             </div>
@@ -35,3 +52,8 @@ function appendData(data) {
     }
     main.innerHTML = jobsHTML;
 }
+
+
+{/* <div class="col-md-4 col-lg-3 job-license">${job.license} permis de conducere</div>
+<div class="col-md-4 col-lg-3 job-country"><i class="fas fa-map-marker-alt"></i>${job.country}</div>
+<div class="col-md-4 col-lg-3 job-payment"><i class="fas fa-dollar-sign"></i>${job.payment}</div> */}
---
+++ js/single.js
@@ -0,0 +1,3 @@
+$.getJSON("https://driveral.com/hu/api/job?uid=3e7ccfff-eab8-40b3-b055-98fbd40c2286&page=1&lang=en&slug=bhs-trans-kft/belfoldi-flottabovites-c-e-kategoria-kiemelt-berezessel", function (data) {
+    console.log(data["data"])
+})
