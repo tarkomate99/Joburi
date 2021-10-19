5302094f36eb72fc0dd9635efa394d9433709e79
tarkomate99 <tarkomate99@gmail.com>
2021-10-19 14:42:50 +0200

    single scss

--- js/single.js
+++ js/single.js
@@ -14,16 +14,7 @@ function appendData(data) {
         <div class="row">
         <div class="col-12 mb-4">
             <div class="row">
-                <div class="col-lg-2" style="border: 1px solid #a2c0be; width:100px; height: 100px; text-align:center;"><img src="${data.company_image}" alt="" class="image" style="max-width: 80%;
-                                                                        max-height: 80%;
-                                                                        border-image-width: 10px;
-                                                                        padding: 10px 10px 10px 10px; vertical-align: middle; horizontal-align: middle; border:none; height:65x; text-align:center;
-                                                                        position: absolute;
-                                                                        top: 50%;
-                                                                        left: 50%;
-                                                                        -moz-transform: translateX(-50%) translateY(-50%);
-                                                                        -webkit-transform: translateX(-50%) translateY(-50%);
-                                                                        transform: translateX(-50%) translateY(-50%);"></div>
+                <div class="col-lg-2" style="border: 1px solid #a2c0be; width:100px; height: 100px; text-align:center;"><img src="${data.company_image}" alt="" class="image"></div>
                                                                         <div class="col-lg-10 d-flex align-items-center">
                                                                             <div class="row ml-1">
                                                                                 <div class="col-12 job-title">
